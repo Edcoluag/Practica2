abfd4f64b4173fa2530291fd7c3e9aa80b3708a4
Edcoluag <155853509+Edcoluag@users.noreply.github.com>
2024-01-09 22:38:09 -0500

    Update cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "MINISTRO DE CULTURA",
+"autor" => "UN ANONIMO",
+"resumen" => "MINISTRO DE CULTURA MUERE EN LETHAL COMPANY",
 ];
 ?>
