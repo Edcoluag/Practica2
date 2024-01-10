95fe9fb3297bc12d30dd1b8145579da7755cc6fb
Edcoluag <155853509+Edcoluag@users.noreply.github.com>
2024-01-09 22:09:04 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Robo de Panqueques",
+"autor" => "Bajito",
+"resumen" => "Se robo los panqueques",
 ];
 ?>
