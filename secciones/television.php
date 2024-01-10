9d9481f3882c6ba1aad8d600de7e8962d721c5a9
Edcoluag <155853509+Edcoluag@users.noreply.github.com>
2024-01-09 22:41:02 -0500

    Update television.php

--- secciones/television.php
+++ secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "VIDEO ESCANDALO",
+"autor" => "BAJITO",
+"resumen" => "SE VIRALIZA VIDEO DE BAJITO CON CASTILLOOOOOO!",
 ];
 ?>
