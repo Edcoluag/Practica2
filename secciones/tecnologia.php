e6d66742c037a28c61ca6cc7922e1f0bbaa68f6e
Edcoluag <155853509+Edcoluag@users.noreply.github.com>
2024-01-09 22:36:38 -0500

    Update tecnologia.php

--- secciones/tecnologia.php
+++ secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "DRONES DE LA NASA",
+"autor" => "POLAR",
+"resumen" => "POLAR SE ROBO LOS DRONES",
 ];
 ?>
